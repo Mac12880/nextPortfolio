3ab0739b548728bc31666f57b0b471720bf0c8e8
Mac12880 <marc.juan6.mj@gmail.com>
2023-07-26 17:19:58 +0800

    create a proj with tailwind.css

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Hello world!</h1>
+    </>
+  );
+};
+
+export default About;
